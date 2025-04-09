7a0957ecfb205fa71a821f6b943a1c0b1d7f8e5b
TechSanzo <khauzair679@gmail.com>
2025-04-09 15:37:25 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
   document.getElementById('root') // Must match HTML
 );
